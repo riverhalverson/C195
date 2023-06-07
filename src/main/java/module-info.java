08814fac04b38b1c24a91477004d02e6d67dd675
River Halverson <rhalve3@student.wgu.edu>
2023-06-07 08:41:18 -0600

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module halverson.c195 {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens halverson.c195 to javafx.fxml;
+    exports halverson.c195;
+}
