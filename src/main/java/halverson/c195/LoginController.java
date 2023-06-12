da6686b9c3b2edabb632e42d4a7def2144a29b2c
River Halverson <rhalve3@student.wgu.edu>
2023-06-12 12:50:28 -0600

    Language detection and country display done, need to finish login verification and error messages

--- src/main/java/halverson/c195/LoginController.java
+++ src/main/java/halverson/c195/LoginController.java
@@ -4,22 +4,63 @@ import halverson.c195.helper.DisplayAlert;
 import halverson.c195.helper.GetId;
 import halverson.c195.helper.JDBC;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
+import java.net.URL;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
-public class LoginController {
+public class LoginController implements Initializable {
     public TextField userNameField;
     public TextField passwordField;
     public Label locationLabel;
+    public Text loginLabel;
+    public Text usernameLabel;
+    public Text passwordLabel;
+    public Button loginbuttonLabel;
+    public Text yourlocationLabel;
+
+    public static boolean english = true;
+    public static boolean french = false;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ResourceBundle rb = ResourceBundle.getBundle("halverson.c195/Languages_fr", Locale.getDefault());
+        ResourceBundle rl = ResourceBundle.getBundle("halverson.c195/Locations");
+        
+
+        String country = Locale.getDefault().getCountry();
+
+        if(Locale.getDefault().getLanguage().equals("fr")){
+            french = true;
+            english = false;
+            loginLabel.setText(rb.getString("login"));
+            usernameLabel.setText(rb.getString("username"));
+            passwordLabel.setText(rb.getString("password"));
+            loginbuttonLabel.setText(rb.getString("login"));
+            yourlocationLabel.setText(rb.getString("yourlocation"));
+        }
+
+        locationLabel.setText(country);
+
+
+
+
+    }
 
     public void OnLoginClick(ActionEvent actionEvent) throws SQLException {
         String userName = "";
         String password = "";
         int userId = 0;
+
 
         userName = userNameField.getText();
         password = passwordField.getText();
@@ -48,4 +89,6 @@ public class LoginController {
         }
 
     }
+
+
 }
--- src/main/java/halverson/c195/Main.java
+++ src/main/java/halverson/c195/Main.java
@@ -8,24 +8,23 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.SQLException;
-
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 
 public class Main extends Application {
 
-    public static boolean english = true;
-    public static boolean french = false;
-
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 430, 570);
+        Scene scene = new Scene(fxmlLoader.load(), 437, 570);
         stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) throws SQLException {
+
         JDBC.openConnection();
         launch();
         JDBC.closeConnection();
